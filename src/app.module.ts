aa442afaea7fbdcd29d69ac0bb13aa77bf4ea733
akhrorbek <akhrornarzullayev.na@gmail.com>
2023-02-27 23:47:26 +0500

    init:added Prisma config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { config } from './config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(config)]
+  imports: [ConfigModule.forRoot(config), PrismaModule]
 })
 export class AppModule {}
---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config'
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+    constructor(config: ConfigService) {
+        super({
+            dataSource: {
+                db: {
+                    url:config.getOrThrow<string>('database.url')
+                }
+            }
+        })
+    }
+
+    onModuleInit():Promise<void> {
+        return this.$connect()
+    }
+
+    onModuleDestroy():Promise<void> {
+        return this.$disconnect()
+    }
+}
